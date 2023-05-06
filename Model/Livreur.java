89742685cf785d3d353bd2deb3c4e2b6965e0f21
SEiKiGitHub <86665373+SEiKiGitHub@users.noreply.github.com>
2023-05-06 18:47:46 +0200

    Update Livreur.java

--- Model/Livreur.java
+++ Model/Livreur.java
@@ -15,7 +15,7 @@ public class Livreur {
     
     private String generateRandomName() {
         Random rand = new Random();
-        String[] noms = {"Jean", "Luc", "Pierre", "Julie", "Marie", "Samantha", "Francois", "David"};
+        String[] noms = {"Jean", "Luc", "Pierre", "Julie", "Marie", "Samantha", "François", "David"};
         int randomIndex = rand.nextInt(noms.length);
         return noms[randomIndex];
     }
@@ -50,5 +50,29 @@ public class Livreur {
     
     public void setCommande(Commande commande) {
         this.commande = commande;
+    }
+    
+    // Ajout de la fonctionnalité de gratuité si le livreur arrive après 30 minutes
+    public boolean isLate() {
+        if (commande == null) {
+            return false;
+        }
+        int tempsLivraison = vehicule.getTempsLivraison(commande.getDistance());
+        int tempsAttente = commande.getTempsAttente();
+        int tempsTotal = tempsLivraison + tempsAttente;
+        return tempsTotal > 30;
+    }
+    
+    // Ajout de la fonctionnalité de pizza offerte après la 10ème commande
+    public boolean is10thOrder() {
+        if (commande == null) {
+            return false;
+        }
+        Client client = commande.getClient();
+        if (client == null) {
+            return false;
+        }
+        int numberOfOrders = client.getNumberOfOrders();
+        return numberOfOrders % 10 == 0 && numberOfOrders > 0;
     }
 }

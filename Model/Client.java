182ee7bd7fbbd6714833470f19d9eb668e6cc89e
SEiKiGitHub <86665373+SEiKiGitHub@users.noreply.github.com>
2023-05-06 17:20:17 +0200

    Client.java

--- Model/Client.java
+++ Model/Client.java
@@ -8,6 +8,8 @@ private String address;
 private int balance;
 private int freePizzas;
 
+typescript
+Copy code
 public Customer(String name, String phone, String address) {
     this.name = name;
     this.phone = phone;
@@ -49,3 +51,22 @@ public void addFreePizza() {
 }   
 }
 
+class OrderItem {
+private String name;
+private int quantity;
+
+arduino
+Copy code
+public OrderItem(String name, int quantity) {
+    this.name = name;
+    this.quantity = quantity;
+}
+
+public String getName() {
+    return name;
+}
+
+public int getQuantity() {
+    return quantity;
+}
+}
